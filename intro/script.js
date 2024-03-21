e5932581775e2aa43d48728ab3e90cede9c2bf62
Pritam Kumar <pritamkunar13@gmail.com>
2024-03-21 21:17:04 +0530

    introduction

---
+++ intro/script.js
@@ -0,0 +1,25 @@
+// array object destructring import exports
+// map filter arrow ffunction (implicit return value) spread operator
+
+// arrays
+// array aisa data structure hai jo ki jaada elements rakh sakta hai
+// [1,2,3,4,5,6,7,8,9,10,11]
+// [1,2,3,4,"apple"]
+
+// immutable vs mutable
+
+// var arr = [1,2,3,4];
+// var arr2 = arr;
+// arr2.pop();
+
+
+
+// react js mein ek state naam ki cheej milegi us bande ko 
+// app mutate nhi  krskte matlab ki directly uski value nhi hata sakte ya jo sakt
+
+// var state = [1,2,3];
+// state.pop(); ---->not allow to change state value directly ---->
+ 
+
+// aur ab jo padhana hai wo is baare me hogi ki state ko immutable way mein kaise update karein
+
